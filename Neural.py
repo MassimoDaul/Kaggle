80b3a8d4cca7a36daf388fecc24f6dc05634673c
Massimo Daul <21mdaul@mdirss.org>
2018-01-11 23:03:03 -0500

    Update Neural.py

--- Neural.py
+++ Neural.py
@@ -1,31 +1,10 @@
-from Sentiment_Analysis import sentiment, totalsentiment
-import NetTest_From_Web
-import tensorflow as tf
-
-
-# update tensorflow to cooroperte with 3.6:
-# python3 -m pip install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.12.0-py3-none-any.whl
-
-
-comment = "statement to analyze"
-
-
-# saving sentiment stats as a list.
-sentList = (sentiment(comment))
-
-# threshold for comparison
-
-threshold = totalsentiment(comment)
-
-
-# TODO
-
 """
 
-Tensorflow tutorial
+Massimo Daul
 
-Either find way to adapt data to work for built neural net or use tensorflow to try to predict if the comment should be 
-censored or not
+
+program to analyze web page comment and determine if it should be censored. Trained net on wikipedia comments from a
+kaggle data base.
 
 
 Structure of net:
@@ -43,13 +22,115 @@ Output:
 
 1 - censor
 
-Use data from csv file in repository to get comments.
-
-Manually train, use human judgement to see if comment is appropriate. 
-Try to be consistent (Considering other ways of training, that will be more time efficient and correct. 
-Hard because if we have something that can do it, we have our job done)
+Uses data from csv file in repository to get comments.
 
 """
 
+from Sentiment_Analysis import sentiment, totalsentiment
+import NetTest_From_Web
+import tensorflow as tf
+import pandas as pd
+import csv
+from numpy import exp, array, random, dot
 
+# populating our list of toxic comments using the csv file:
+
+ToxicComments = []
+CleanComments = []
+
+with open("/Users/massimodaul/Downloads/train-2.csv", "r") as train_df:
+    reader = csv.DictReader(train_df)
+
+    for row in reader:
+        if (row['toxic'] == "1" or row['severe_toxic'] == "1" or row['obscene'] == "1" or row['threat'] == "1"
+                or row['insult'] == "1"  or row['identity_hate'] == "1"):
+
+            ToxicComments.append(row['comment_text'])
+
+    for row in reader:
+        if (row['toxic'] == "0" and row['severe_toxic'] == "0" and row['obscene'] == "0" and row['threat'] == "0"
+                and row['insult'] == "0" and row['identity_hate'] == "0"):
+
+            CleanComments.append(row['comment_text'])
+
+
+class NeuralNetwork():
+    def __init__(self):
+        # __init__ runs every time a class NerualNetwork is created, this needs to happen for the nums to be consistent
+        # Seed the random number generator, so it generates the same numbers
+        # every time the program runs.
+        # random.seed(1)
+
+        # We model a single neuron, with 6 input connections and 1 output connection.
+        # We assign random weights to a 6 x 1 matrix, with values in the range -1 to 1
+        # and mean 0.
+        self.synaptic_weights = 2 * random.random((6, 1)) - 1
+
+    # The Sigmoid function, which describes an S shaped curve.
+    # We pass the weighted sum of the inputs through this function to
+    # normalise them between 0 and 1.
+    def __sigmoid(self, x):
+        # @staticmethod
+        return 1 / (1 + exp(-x))
+
+    # The derivative of the Sigmoid function.
+    # This is the gradient of the Sigmoid curve.
+    # It indicates how confident we are about the existing weight.
+    def __sigmoid_derivative(self, x):
+        return x * (1 - x)
+
+    # We train the neural network through a process of trial and error.
+    # Adjusting the synaptic weights each time.
+    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+        for i in range(0, number_of_training_iterations):
+            # Pass the training set through our neural network (a single neuron).
+            output = self.think(training_set_inputs)
+
+            # Calculate the error (The difference between the desired output
+            # and the predicted output).
+            error = training_set_outputs - output
+
+            # Multiply the error by the input and again by the gradient of the Sigmoid curve.
+            # This means less confident weights are adjusted more.
+            # This means inputs, which are zero, do not cause changes to the weights.
+            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
+
+            # Adjust the weights.
+            self.synaptic_weights += adjustment
+
+    # The neural network thinks.
+    def think(self, inputs):
+        # if inputs.all() == 0:
+            # return 0
+
+        # Pass inputs through our neural network (our single neuron).
+        return self.__sigmoid(dot(inputs, self.synaptic_weights))
+
+
+# Initialise a single neuron neural network.
+neural_network = NeuralNetwork()
+
+print("Random starting synaptic weights: ")
+print(neural_network.synaptic_weights)
+
+training_set = []
+
+for i in range(0, len(ToxicComments)):
+
+
+# TODO
+
+# Pass in our values to the array for the neural net. 6 inputs - 1 output.
+
+
+# Train the neural network using a training set.
+# Do it 10,000 times and make small adjustments each time.
+neural_network.train(training_set_inputs, training_set_outputs, 100000)
+
+print("New synaptic weights after training: ")
+print(neural_network.synaptic_weights)
+
+# Test the neural network with a new situation.
+print("New condition:")
+print(float(neural_network.think(array([]))))
 
